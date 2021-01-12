d2648cebae3230ea7947476b5a04b20589e5dc7c
Uduak Essien <acushla4real@gmail.com>
2021-01-12 15:00:30 +0100

    Update index.js with axios defaults

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+axios.defaults.baseURL='http://localhost:3001/api/v1';
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem("token")}`
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +14,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
